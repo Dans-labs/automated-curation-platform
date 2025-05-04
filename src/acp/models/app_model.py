29d8353fe751482ab0cc6eab722284855ece00df
ekoi <eko.indarto@dans.knaw.nl>
2025-05-04 09:46:36 +0200

    Add documentation

--- src/acp/models/app_model.py
+++ src/acp/models/app_model.py
@@ -53,49 +53,44 @@ class InboxDatasetDataModel:
 class TargetApp(BaseModel):
     """
     Data model for target application.
-    The TargetApp class is designed to hold various attributes related to a target application, such as repo_name,
-    display_name, deposit_status, deposit_time, duration, and output_response. These attributes are used
-    to store information about the repository, its display name, the status and time of deposits,
-    the duration of processes, and the output response from the target application.
-    By including the diff attribute, the TargetApp class can also keep track of any differences or changes
-    that may occur within the target application, providing a flexible way to store and manage this information.
 
     Attributes:
-        repo_name (str): The name of the repository, aliased as 'repo-name'.
-        display_name (str): The display name of the target application, aliased as 'display-name'.
-        deposit_status (str): The deposit status of the target application, aliased as 'deposit-status'.
-        deposited_at (str): The deposit time of the target application, aliased as 'deposit-time'.
-        duration (str): The duration of the process.
-        output_response (Dict[str, Any]): The output response from the target application, aliased as 'output-response'.
+        repo_name (str): Repository name, aliased as 'repo-name'.
+        display_name (str): Display name of the target application, aliased as 'display-name'.
+        deposit_status (str): Deposit status, aliased as 'deposit-status'.
+        deposited_at (datetime | str): Deposit time, aliased as 'deposit-time'.
+        deposit_duration (float | str): Duration of the deposit process.
+        output_response (dict): Output response from the target application, aliased as 'output-response'.
+        external_identifiers (Union[str, List[dict]]): External identifiers, aliased as 'deposited-identifiers'.
+        diff (dict): Differences or changes in the target application.
     """
     repo_name: str = Field(None, alias='repo-name')
     display_name: str = Field(None, alias='display-name')
     deposit_status: str = Field(None, alias='deposit-status')
     deposited_at: datetime | str = Field(None, alias='deposit-time')
-    deposit_duration: float|str = ''
+    deposit_duration: float | str = ''
     output_response: dict = Field(None, alias='output-response')
-    external_identifiers: Union[str,List[dict]] = Field(None, alias='deposited-identifiers')
+    external_identifiers: Union[str, List[dict]] = Field(None, alias='deposited-identifiers')
     diff: dict = {}
-
 
 class Asset(BaseModel):
     """
     Data model for an asset.
-    The Asset class is a data model defined using the BaseModel class from the pydantic library.
-    This class is designed to represent an asset with various attributes related to its metadata and
-    associated target applications.
-    The class includes several attributes, each with specific types and optional aliases.
+
+    The Asset class is a data model defined using the BaseModel class from the Pydantic library.
+    It represents an asset with various attributes related to its metadata and associated target applications.
 
     Attributes:
         dataset_id (str): The ID of the dataset, aliased as 'dataset-id'.
         title (str): The title of the asset.
-        metadata_content (str): The metadata of the asset.
-        created_at (str): The creation date of the asset, aliased as 'created-date'.
-        saved_at (str): The saved date of the asset, aliased as 'saved-date'.
-        submitted_at (str): The submitted date of the asset, aliased as 'submitted-date'.
-        status (str): The release version of the asset, aliased as 'release-version'.
-        version (str): The version of the asset.
-        targets (List[TargetApp]): The list of target applications associated with the asset.
+        md (dict | str): The metadata content of the asset, which can be a dictionary or a string.
+        created_at (datetime | str): The creation date of the asset, aliased as 'created-at'.
+        saved_at (datetime | str): The saved date of the asset, aliased as 'saved-at'.
+        submitted_at (datetime | str): The submitted date of the asset, aliased as 'submitted-at'.
+        deposited_version (str): The deposited version of the asset, aliased as 'deposited-version'.
+        status (str): The status of the asset, aliased as 'status'.
+        acp_version (str): The ACP version of the asset, aliased as 'acp-version'.
+        targets (List[TargetApp]): A list of TargetApp objects representing the target applications associated with the asset.
     """
     dataset_id: str = Field(None, alias='dataset-id')
     title: str = ''
@@ -112,14 +107,13 @@ class Asset(BaseModel):
 class OwnerAssetsModel(BaseModel):
     """
     Data model for owner assets.
-    The OwnerAssetsModel class is a data model defined using the BaseModel class from the pydantic library.
-    This class is designed to represent the assets owned by a specific owner, encapsulating the owner's ID and a list of assets.
-    The class includes two main attributes: owner_id and assets. The owner_id attribute is a string that is aliased
-    as owner-id, which helps in mapping the attribute to a different name when working with external data sources
+
+    This class represents the assets owned by a specific owner, encapsulating the owner's ID and a list of assets.
+    It is defined using the Pydantic `BaseModel` class, which provides data validation and serialization.
 
     Attributes:
-        owner_id (str): The ID of the owner, aliased as 'owner-id'.
-        assets (List[Asset]): The list of assets owned by the owner.
+        owner_id (str): The ID of the owner. This field is aliased as 'owner-id' for compatibility with external data sources.
+        assets (List[Asset]): A list of `Asset` objects representing the assets owned by the owner.
     """
-    owner_id: str = Field(None, alias='owner-id')
-    assets: List[Asset] = []
+    owner_id: str = Field(None, alias='owner-id')  # The owner's ID, aliased as 'owner-id'.
+    assets: List[Asset] = []  # A list of assets owned by the owner.
